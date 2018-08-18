9cf6b6f015506313a1c2d986d8530154b12186b6
jackhman <shengxu19910830@163.com>
2018-08-18 14:20:20 +0800

    更新let1.js文件内容

--- let/let1.js
+++ let/let1.js
@@ -1,5 +1,17 @@
 var a='a';
 function funA() {
+    console.log(cc);
     console.log(a);
+    var b='b';
+    console.log(b);
+
 }
 funA();
+{
+    console.log("----"+c);
+    let c='c';
+    console.log(c);
+}
+
+// console.log(b);
+console.log(c);
